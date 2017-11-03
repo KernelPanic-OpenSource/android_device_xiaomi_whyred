21d21734fb261040f2894a7856bd341d95506af7
Chris Fries <cfries@google.com>
2017-11-03 11:08:45 -0500

    whyred: Set printk.devkmsg=on on kernel cmdline to avoid ratelimit
    init and friends are getting ratelimted on /dev/kmsg.  Let's keep
    the logs by setting printk.devkmsg=on.
    
    Bug: 62952768
    Test: Check for init logs in kernel during boot
    Change-Id: I103e3cc1d73880af1d9a0cdc4dae05223eb4c427
    Signed-off-by: ItsVixano <vixanonew@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
+BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
@@ -63,7 +64,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/whyred
-TARGET_KERNEL_CONFIG := whyred-oldcam_defconfig
+TARGET_KERNEL_CONFIG := whyred-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 
 # QCOM
