6c29549a8bc4399b20e18926e64da1618347c3f6
dopaemon <polarisdp@gmail.com>
2021-06-12 21:00:57 +0700

    Derp CherishOS -> AOSP
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -34,7 +34,7 @@ PRODUCT_GENERIC_PROPERTIES += \
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := cherish_whyred
+PRODUCT_NAME := aosp_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
